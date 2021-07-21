b92a6872b8aa8a7a001a32056cf3999cbbb16668
hts_0000 <hts_0000@sina.com>
2021-07-21 17:34:00 +0800

    更新8.1章节

---
+++ 8/8.2/clock2/clock2.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8001")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+var connN = 0
+
+func handleConn(c net.Conn) {
+	defer func() {
+		connN++
+		log.Printf("conn = %d\n", connN)
+		c.Close()
+	}()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
