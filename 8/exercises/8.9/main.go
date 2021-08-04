05b7b1633d6a9598a71e63a174cf8ecb5d92a68b
hts_0000 <hts_0000@sina.com>
2021-08-04 18:06:58 +0800

    更新

--- 8/exercises/8.9/main.go
+++ 8/exercises/8.9/main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"sort"
 )
 
-// 练习 8.9
 // 编写一个du工具，每隔一段时间将root目录下的目录大小计算并显示出来
 
+var (
+	sema = make(chan struct{}, 20)
+)
+
 func main() {
-	entries, err := ioutil.ReadDir("/root")
+	rootPath := "/root"
+	entries, err := ioutil.ReadDir(rootPath)
 	if os.IsPermission(err) { // "permission denied"
 		fmt.Fprintf(os.Stderr, "du: %v\n", err)
 		return
@@ -22,12 +28,39 @@ func main() {
 	dirs := make([]string, 0)
 	for _, entrie := range entries {
 		if entrie.IsDir() {
-			dirs = append(dirs, entrie.Name())
+			dirs = append(dirs, filepath.Join(rootPath, entrie.Name()))
 		}
 	}
+	sort.Strings(dirs)
 	for _, dir := range dirs {
 		fmt.Println(dir)
 	}
+}
+
+func dirSize(dir string, dirsSize map[string]int64) {
+	sema <- struct{}{}
+	defer func() { <-sema }()
+	dirsSize[dir] = size(dir)
+}
+
+func size(dir string) int64 {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subdir := filepath.Join(dir, entry.Name())
+			size(subdir)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+func dirents(dir string) []os.FileInfo {
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "dirents: %v\n", err)
+		return nil
+	}
+	return entries
 }
 
 // 1. 获取root下所有目录（权限判断），存储在[]string里（每隔一段时间重新获取），并排序
